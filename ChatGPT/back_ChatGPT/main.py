a20fc0e28b625a8874017960e3e6d8dc286bb727
igorayelloborgesdev <igorayelloborgesdev@gmail>
2025-03-03 03:44:55 -0300

    reset v1

--- ChatGPT/back_ChatGPT/main.py
+++ ChatGPT/back_ChatGPT/main.py
@@ -1,18 +1,7 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException, Depends
-from application.use_cases.ExcelProcessorUseCase import ExcelProcessorUseCase
-from infrastructure.database.database import get_db
-from sqlalchemy.ext.asyncio import AsyncSession
-from infrastructure.repositories.segmento_classificacao_repository import SegmentoClassificacaoRepository
+from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():    
     return {"Hello": "World"}
-
-@app.post("/upload-excel/")
-async def upload_excel(file: UploadFile = File(...), db: AsyncSession = Depends(get_db)): 
-    repository = SegmentoClassificacaoRepository(db)   
-    excel_processor = ExcelProcessorUseCase(repository)
-    await excel_processor.processar_dados(file)    
-    return {"TESTE"}
